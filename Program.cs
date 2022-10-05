3515d4e2129b6ca4a72bbd0ffeed5bc4432c30fa
FibonacciFox <shamanwolfrus@gmail.com>
2022-10-06 01:56:00 +0700

    Добавлено получение Свойств

--- Program.cs
+++ Program.cs
@@ -1,31 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Reflection;
+using Apf.Controls;
+
+
+//Assembly assembly = Assembly.Load("Avalonia.Controls");
+//Type myType = assembly.GetType("Avalonia.Controls.Window");
+
+Type myType = typeof(UxButton);
 
 
 
-Assembly assembly = Assembly.Load("Avalonia.Controls");
 
-//Type myType = typeof(TemplatedControl);
-
-Type myType = assembly.GetType("Avalonia.Controls.Window");
-
-
-Console.WriteLine($"Is class: {myType.IsClass}");       // является ли тип классом
-Console.WriteLine($"Имя Класса: {myType.Name}");              // получаем краткое имя типа
+Console.WriteLine($"Является ли тип классом: {myType.IsClass}");
+Console.WriteLine($"Имя Класса: {myType.Name}"); 
 Console.WriteLine($"Наследуется от: {myType.BaseType}");
-Console.WriteLine($"Full Name: {myType.FullName}");     // получаем полное имя типа
-Console.WriteLine($"Namespace: {myType.Namespace}");    // получаем пространство имен типа
-Console.WriteLine($"Is struct: {myType.IsValueType}");  // является ли тип структурой
+Console.WriteLine($"Полное имя типа: {myType.FullName}");
+Console.WriteLine($"Пространство имен типа: {myType.Namespace}");
 Console.WriteLine("--------------------------------------"); 
 
-foreach (MemberInfo member in myType.GetMembers( BindingFlags.Instance | BindingFlags.Public))
+foreach (PropertyInfo prop in myType.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static))
 {
-    if (member.MemberType == MemberTypes.Property)
+    if (prop.MemberType == MemberTypes.Property)
     {
-        //var ageProp = myType.GetProperty(member.Name);
-        Console.WriteLine($"Унаследовано от: {member.DeclaringType} Значение типа: {member.MemberType} Имя свойства: {member.Name}");
+        Console.WriteLine($"[Унаследовано от:{prop.DeclaringType}] [Значение типа:{prop.MemberType}] Тип: {prop.PropertyType} [Имя свойства:{prop.Name}]");
     }
+    
 }
 
 
